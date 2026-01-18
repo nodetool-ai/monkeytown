8d71f953b7aece47b7e64905756a77bfa8d9a76c
georgi <matti.georgi@gmail.com>
2026-01-18 07:24:48 +0100

    nextjs boilerplate

--- web/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { SystemPulse } from './SystemPulse';
-export { AgentCard } from './AgentCard';
-export { TerrariumView } from './TerrariumView';
-export { GhostColumn } from './GhostColumn';
-export { FlowStream } from './FlowStream';
-export { ActionSeed } from './ActionSeed';
-export { DetailPanel } from './DetailPanel';
-export { ErrorCard, InlineError } from './ErrorCard';
--- web/src/test/setup.ts
+++
@@ -1,2 +0,0 @@
-Object.defineProperty(HTMLElement.prototype, 'offsetWidth', { configurable: true, value: 100 });
-Object.defineProperty(HTMLElement.prototype, 'offsetHeight', { configurable: true, value: 100 });
